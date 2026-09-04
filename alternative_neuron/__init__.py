d7f3018d2e6d96a4e6e92b877c074eb211e545d1
anttiluode <67085641+anttiluode@users.noreply.github.com>
2026-09-04 08:53:36 +0300

    Expose AlternativeNeuron core

---
+++ alternative_neuron/__init__.py
@@ -0,0 +1,23 @@
+from .core import (
+    ActivePoker,
+    AlternativeNeuron,
+    Identification,
+    MediumMemory,
+    PokeWorld,
+    SlowStructure,
+    StepResult,
+    silent_switch_sequence,
+    visible_event_sequence,
+)
+
+__all__ = [
+    "ActivePoker",
+    "AlternativeNeuron",
+    "Identification",
+    "MediumMemory",
+    "PokeWorld",
+    "SlowStructure",
+    "StepResult",
+    "silent_switch_sequence",
+    "visible_event_sequence",
+]
